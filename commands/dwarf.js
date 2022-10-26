3a807982ec4ea988b4e2285d221023f0e7598e8e
Jonas Berx <jonas.berx@ut.ee>
2022-10-26 20:18:57 +0300

    buttons and modals yk

--- commands/dwarf.js
+++ commands/dwarf.js
@@ -1,13 +1,21 @@
 const { SlashCommandBuilder } = require('discord.js');
+const {TENOR_API_TOKEN, GOOGLE_API_TOKEN} = require('../config.json')
+const fetch = require("node-fetch");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('dwarf')
         .setDescription('A mighty dwarf will appear'),
     async execute(interaction) {
-        return interaction.reply({
-            content:'shhh, im working here',
-            ephemeral: true
-        });
+        const url = "https://tenor.googleapis.com/v2/search?q=wowdwarf&key=" +
+            TENOR_API_TOKEN +"&client_key=" + GOOGLE_API_TOKEN +  "&limit=20";
+
+        const res = await fetch(url);
+
+        const result = await res.json()
+
+        const idx = Math.floor(Math.random() * result.results.length);
+
+        return interaction.reply(result.results[idx].url);
     },
 };
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits, EmbedBuilder, ActivityType, Events, Partials, channelLink} = require('discord.js');
-const { token,reimburseChannel } = require('./config.json');
+const { Client, Collection, GatewayIntentBits, EmbedBuilder, ActivityType, Events, Partials, channelLink,
+    ActionRowBuilder, ButtonBuilder, ButtonStyle,
+} = require('discord.js');
+const { token, access_to_buttons, bounty_channel, guildbank_channel, reimbursement_channel } = require('./config.json');
 
 
 const client = new Client({
@@ -48,19 +50,42 @@ client.on('interactionCreate', async interaction => {
                 {name: "Context", value: "Not provided", inline: false}
             );
         }
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('reimburse_success')
+                    .setLabel('Approved')
+                    .setStyle(ButtonStyle.Success),
+                new ButtonBuilder()
+                    .setCustomId('reimburse_cancelled')
+                    .setLabel('Denied')
+                    .setStyle(ButtonStyle.Danger)
+            );
+
         // Send message to Thane channel to trace the requests that get sent in.
-        client.channels.cache.get(reimburseChannel).send({embeds: [reimburseEmbed]});
+        client.channels.cache.get(reimbursement_channel).send({embeds: [reimburseEmbed], components: [row]});
         await interaction.reply({ content: 'Your submission was received successfully!' });
+
+        const buttonFilter = i => i.customId === 'success' || i.customId==="cancelled" && i.user.id in access_to_buttons;
+        const collector = client.channels.cache.get(reimbursement_channel).createMessageComponentCollector({ buttonFilter, time: 15000 });
+
+        collector.on('collect', async i => {
+            if (i.customId === 'reimburse_success') {
+                row.components[0].setDisabled(true);
+                row.components[1].setDisabled(true);
+                await i.update({ content: '**Approved**', components: [] });
+            }
+            if (i.customId === 'reimburse_cancelled') {
+                row.components[0].setDisabled(true);
+                row.components[1].setDisabled(true);
+                await i.update({ content: '**Denied**', components: [] });
+            }
+        });
     }
     if (interaction.customId === 'bountyModal') {
-
-        const filter = (reaction, user) => {
-            return ['👍', '👎'].includes(reaction.emoji.name) && user.id === interaction.user.id;
-        };
-
         const bounty_name = interaction.fields.getTextInputValue('target_name');
         const bounty_race = interaction.fields.getTextInputValue('target_race');
-        const bounty_reward = interaction.fields.getTextInputValue('target_race');
+        const bounty_reward = interaction.fields.getTextInputValue('target_reward');
 
         const bountyEmbed = new EmbedBuilder()
             .setColor(0x0099FF)
@@ -72,22 +97,40 @@ client.on('interactionCreate', async interaction => {
             .addFields(
                 {name: "Target name", value: bounty_name, inline: false},
                 {name: "Target race", value: bounty_race, inline: false},
-                {name: "Bounty reward", value: bounty_reward, inline: false},
+                {name: "Extra info; Reward / last seen?", value: bounty_reward, inline: false},
             )
             .setFooter({text: "Loremaster Hendrik", iconURL: 'https://i.imgur.com/vYQlnnA.png'})
 
-        client.channels.cache.get('1015742051595329717').send({embeds: [bountyEmbed]}).then(
-            embedMessage => {
-                let reactionEmoji = embedMessage.guild.emojis.cache.find(emoji => emoji.name === 'wow_cross');
-                embedMessage.react(reactionEmoji)
-                    .then(() => {
-                            reactionEmoji = embedMessage.guild.emojis.cache.find(emoji => emoji.name === 'wow_triangle');
-                            embedMessage.react(reactionEmoji)
-                        }
-                    ).catch(err => console.error("something went wrong", err))
-            });
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('bounty_success')
+                    .setLabel('Completed')
+                    .setStyle(ButtonStyle.Success),
+                new ButtonBuilder()
+                    .setCustomId('bounty_cancelled')
+                    .setLabel('Cancelled')
+                    .setStyle(ButtonStyle.Danger)
+            );
 
+        client.channels.cache.get(bounty_channel).send({embeds: [bountyEmbed], components:[row]});
         await interaction.reply({content: 'Bounty has successfully been marked.'});
+
+        const buttonFilter = i => i.customId === 'success' || i.customId==="cancelled" && i.user.id in access_to_buttons;
+        const collector = client.channels.cache.get(bounty_channel).createMessageComponentCollector({ buttonFilter, time: 15000 });
+
+        collector.on('collect', async i => {
+            if (i.customId === 'bounty_success') {
+                row.components[0].setDisabled(true);
+                row.components[1].setDisabled(true);
+                await i.update({ content: '**Completed**', components: [] });
+            }
+            if (i.customId === 'bounty_cancelled') {
+                row.components[0].setDisabled(true);
+                row.components[1].setDisabled(true);
+                await i.update({ content: '**Cancelled**', components: [] });
+            }
+        });
     }
     if (interaction.customId === 'gbrequestModal') {
         const materials = interaction.fields.getTextInputValue('mat_description');
@@ -105,55 +148,39 @@ client.on('interactionCreate', async interaction => {
                     {name:"Justification | Optional", value:justification, inline:false},
                 );
 
-        client.channels.cache.get('1015742051595329717').send({embeds: [materialEmbed]}).then(
-            embedMessage => {
-                let reactionEmoji = embedMessage.guild.emojis.cache.find(emoji => emoji.name === 'wow_cross');
-                embedMessage.react(reactionEmoji)
-                    .then(() => {
-                            reactionEmoji = embedMessage.guild.emojis.cache.find(emoji => emoji.name === 'wow_triangle');
-                            embedMessage.react(reactionEmoji)
-                        }
-                    ).catch(err => console.error("something went wrong", err))
-            });
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('material_success')
+                    .setLabel('Approved')
+                    .setStyle(ButtonStyle.Success),
+                new ButtonBuilder()
+                    .setCustomId('material_cancelled')
+                    .setLabel('Denied')
+                    .setStyle(ButtonStyle.Danger)
+            );
+
+        client.channels.cache.get(guildbank_channel).send({embeds: [materialEmbed], components: [row]});
         await interaction.reply({ content: 'Request is being processed.' });
+
+        const buttonFilter = i => i.customId === 'success' || i.customId==="cancelled" && i.user.id in access_to_buttons;
+        const collector = client.channels.cache.get(guildbank_channel).createMessageComponentCollector({ buttonFilter, time: 15000 });
+
+        collector.on('collect', async i => {
+            console.log(i.user.roles)
+            if (i.customId === 'material_success') {
+                row.components[0].setDisabled(true);
+                row.components[1].setDisabled(true);
+                await i.update({ content: '**Approved**', components: [] });
+            }
+            if (i.customId === 'material_cancelled') {
+                row.components[0].setDisabled(true);
+                row.components[1].setDisabled(true);
+                await i.update({ content: '**Denied**', components: [] });
+            }
+        });
     }
 });
-
-client.on(Events.MessageReactionAdd, async(reaction, user) => {
-    if (reaction.partial) {
-        try {
-            await reaction.fetch();
-
-        } catch (err) {
-            console.log(err)
-            return;
-        }
-        console.log(reaction.message)
-        // TODO also add channel IDS here
-        // TODO WTF IS Going on here?
-        if (reaction.emoji.name === 'anvilguard' && reaction.message.channelId === '999755188837552221' && user.id==='232566719183323136') {
-            reaction.message.reactions.removeAll().catch(err => {
-                console.log("err")})
-            await reaction.message.reply("Bounty has been completed.")
-        }
-        if (reaction.emoji.name === 'anvilguard' && reaction.message.channelId === '1015742051595329717' && user.id==='232566719183323136') {
-            reaction.message.reactions.removeAll().catch(err => {
-                console.log("err")})
-            await reaction.message.reply("Request has been fulfilled.")
-        }
-        if (reaction.emoji.name === 'wow_cross' && reaction.message.channelId === '999755188837552221' && user.id==='232566719183323136') {
-            reaction.message.reactions.removeAll().catch(err => {
-                console.log("err")})
-            await reaction.message.reply("Bounty has been cancelled.")
-
-        }
-        if (reaction.emoji.name === 'wow_cross' && reaction.message.channelId === '1015742051595329717' && user.id==='232566719183323136') {
-            reaction.message.reactions.removeAll().catch(err => {
-                console.log("err")})
-            await reaction.message.reply("Request has been denied.")
-
-        }
-    }});
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
@@ -167,6 +194,37 @@ client.on('interactionCreate', async interaction => {
     } catch (error) {
         console.error(error);
         await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+
+client.on('interactionCreate', async (button) => {
+    if (!button.isButton()) return;
+    if (!button.user.id in access_to_buttons) return;
+    console.log(button.message.components[0].components[0])
+    if (button.customId === "reimburse_cancelled") {
+        button.message.components[0].components[0].data.disabled = true
+        await button.update({ content: '**Denied**', components: [] });
+    }
+    if (button.customId === "material_cancelled") {
+        button.message.components[0].components[0].data.disabled = true
+        await button.update({ content: '**Denied**', components: [] });
+    }
+    if (button.customId === "bounty_cancelled") {
+        button.message.components[0].components[0].data.disabled = true
+        await button.update({ content: '**Cancelled**', components: [] });
+    }
+    if (button.customId === "reimburse_success") {
+        button.message.components[0].components[0].data.disabled = true
+        await button.update({ content: '**Approved**', components: [] });
+    }
+    if (button.customId === "material_success") {
+        button.message.components[0].components[0].data.disabled = true
+        await button.update({ content: '**Approved**', components: [] });
+    }
+    if (button.customId === "bounty_success") {
+        button.message.components[0].components[0].data.disabled = true
+        await button.update({ content: '**Completed**', components: [] });
+
     }
 });
 
